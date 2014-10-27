35140cd40ca53d1ba4e199b64b8a456d6aa72cd6
helisam <helisam@gmail.com>
2014-10-27 10:34:30 -0400

    Criação do repositório do Itinerario

--- appWeb/src/br/com/embarcado/entities/Itinerario.java
+++ appWeb/src/br/com/embarcado/entities/Itinerario.java
@@ -20,15 +20,14 @@ import javax.persistence.TemporalType;
 		@NamedQuery(name = "Itinerario.findAll", query = "SELECT i FROM Itinerario i"),
 		@NamedQuery(name = "Itinerario.count", query = "SELECT COUNT(i) FROM Itinerario i"),
 		@NamedQuery(name = "Itinerario.findByDestino", query = "SELECT i FROM Itinerario i WHERE i.destino = :destino") })
-
 public class Itinerario implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="itinerario_ID")
+	@Column(name = "itinerario_ID")
 	private Long id;
-	
-	@Column(name = "itinerario_DESTINO", length=50, nullable = false)
+
+	@Column(name = "itinerario_DESTINO", length = 50, nullable = false)
 	private String destino;
 	@Column(name = "itinerario_LOCALPARTIDA", nullable = false)
 	private String localPartida;
@@ -41,49 +40,60 @@ public class Itinerario implements Serializable {
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "itinerario_DATA", nullable = false)
 	private Date data;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getDestino() {
 		return destino;
 	}
+
 	public void setDestino(String destino) {
 		this.destino = destino;
 	}
+
 	public String getLocalPartida() {
 		return localPartida;
 	}
+
 	public void setLocalPartida(String localPartida) {
 		this.localPartida = localPartida;
 	}
+
 	public String getHoraChegada() {
 		return horaChegada;
 	}
+
 	public void setHoraChegada(String horaChegada) {
 		this.horaChegada = horaChegada;
 	}
+
 	public String getDistancia() {
 		return distancia;
 	}
+
 	public void setDistancia(String distancia) {
 		this.distancia = distancia;
 	}
+
 	public String getObservacao() {
 		return observacao;
 	}
+
 	public void setObservacao(String observacao) {
 		this.observacao = observacao;
 	}
+
 	public Date getData() {
 		return data;
 	}
+
 	public void setData(Date data) {
 		this.data = data;
 	}
-	
-	
 }
---
+++ appWeb/src/br/com/embarcado/repository/ItinerarioRepository.java
@@ -0,0 +1,54 @@
+package br.com.embarcado.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import br.com.embarcado.entities.Itinerario;
+
+public class ItinerarioRepository {
+	private EntityManager entityManager;
+	
+	public ItinerarioRepository(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	public void save(Itinerario itinerario) {
+		this.entityManager.persist(itinerario);
+		this.entityManager.flush();
+	}
+
+	public void remove(Itinerario itinerario) {
+		Itinerario itinerarioTemp = new Itinerario();
+		itinerarioTemp = this.entityManager.find(Itinerario.class, itinerario.getId());
+		this.entityManager.remove(itinerarioTemp);
+	}
+	
+	public void update(Itinerario itinerario){
+		this.entityManager.merge(itinerario);
+		this.entityManager.flush();
+	}
+	
+	public Itinerario find(Long ID) {
+		return this.entityManager.find(Itinerario.class, ID);
+	}
+
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public List getItinerarios() {
+		return this.entityManager.createNamedQuery("Itinerario.findAll")
+				.getResultList();
+	}
+
+	public Long getCountItinerarios() {
+		return (Long) this.entityManager.createNamedQuery("Itinerario.count")
+				.getSingleResult();
+	}
+}
