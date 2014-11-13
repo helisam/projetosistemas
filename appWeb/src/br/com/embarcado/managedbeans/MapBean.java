2e56bf1ee699ca15205e24feb3fad56b90fa7db0
helisam <helisam@gmail.com>
2014-11-12 22:02:23 -0400

    Ajustando o gmap, adicionado o script no layout.xhtml para fazer funcionar o gmap corretamrnte

--- appWeb/src/br/com/embarcado/managedbeans/MapBean.java
+++ appWeb/src/br/com/embarcado/managedbeans/MapBean.java
@@ -2,7 +2,6 @@ package br.com.embarcado.managedbeans;
 
 import java.io.Serializable;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -18,36 +17,36 @@ import org.primefaces.model.map.Marker;
 public class MapBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-    private MapModel advancedModel;
-    
-    private Marker marker;
-  
-    @PostConstruct
-    public void init() {
-        advancedModel = new DefaultMapModel();
-          
-        //Shared coordinates
-        LatLng coord1 = new LatLng(36.879466, 30.667648);
-        LatLng coord2 = new LatLng(36.883707, 30.689216);
-        LatLng coord3 = new LatLng(36.879703, 30.706707);
-        LatLng coord4 = new LatLng(36.885233, 30.702323);
-          
-        //Icons and Data
-        advancedModel.addOverlay(new Marker(coord1, "Konyaalti", "konyaalti.png", "http://maps.google.com/mapfiles/ms/micons/blue-dot.png"));
-        advancedModel.addOverlay(new Marker(coord2, "Ataturk Parki", "ataturkparki.png"));
-        advancedModel.addOverlay(new Marker(coord4, "Kaleici", "kaleici.png", "http://maps.google.com/mapfiles/ms/micons/pink-dot.png"));
-        advancedModel.addOverlay(new Marker(coord3, "Karaalioglu Parki", "karaalioglu.png", "http://maps.google.com/mapfiles/ms/micons/yellow-dot.png"));
-    }
-  
-    public MapModel getAdvancedModel() {
-        return advancedModel;
-    }
-      
-    public void onMarkerSelect(OverlaySelectEvent event) {
-        marker = (Marker) event.getOverlay();
-    }
-      
-    public Marker getMarker() {
-        return marker;
-    }
+	private MapModel advancedModel;
+
+	private Marker marker;
+
+	public MapBean() {
+		advancedModel = new DefaultMapModel();
+		
+		//Shared coordinates
+		LatLng coord1 = new LatLng(36.879466, 30.667648);
+		LatLng coord2 = new LatLng(36.883707, 30.689216);
+		LatLng coord3 = new LatLng(36.879703, 30.706707);
+		LatLng coord4 = new LatLng(36.885233, 30.702323);
+		
+		//Icons and Data
+		advancedModel.addOverlay(new Marker(coord1, "Konyaalti", "konyaalti.png", "http://maps.google.com/mapfiles/ms/micons/blue-dot.png"));
+		advancedModel.addOverlay(new Marker(coord2, "Ataturk Parki", "ataturkparki.png"));
+		advancedModel.addOverlay(new Marker(coord4, "Kaleici", "kaleici.png", "http://maps.google.com/mapfiles/ms/micons/pink-dot.png"));
+		advancedModel.addOverlay(new Marker(coord3, "Karaalioglu Parki", "karaalioglu.png", "http://maps.google.com/mapfiles/ms/micons/yellow-dot.png"));
+	}
+
+	public MapModel getAdvancedModel() {
+		return advancedModel;
+	}
+	
+	public void onMarkerSelect(OverlaySelectEvent event) {
+		marker = (Marker) event.getOverlay();
+//				.getMarker();
+	}
+	
+	public Marker getMarker() {
+		return marker;
+	}
 }
